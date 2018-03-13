2aa2b23a9b5e14d90c0eb479c99fb1269535a66b
rlin <rlin@thoughtworks.com>
2018-03-13 18:52:09 +0800

    [@boydfd] add fail test to make test report show something.

--- src/test/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplicationTests.java
+++ src/test/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplicationTests.java
@@ -5,12 +5,19 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JenkinsDockerDemoApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void shouldFail() {
+        assertEquals("expected value","wrong value");
+    }
 
 }
