e56b0b18fc3a07ce7696edd81d6986789d8ca802
rlin <rlin@thoughtworks.com>
2018-03-13 17:01:58 +0800

    [@boydfd] add spring structure.

---
+++ src/main/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.aboydfd.jenkins_docker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aboydfd.jenkins_docker_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsDockerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
