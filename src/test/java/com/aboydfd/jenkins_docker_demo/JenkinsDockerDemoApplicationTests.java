cb82d190b8cfdf17ced2ece62b5d69f8b3af81ed
rlin <rlin@thoughtworks.com>
2020-02-16 16:00:21 +0800

    [@boydfd] test for fail.

--- src/test/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplicationTests.java
+++ src/test/java/com/aboydfd/jenkins_docker_demo/JenkinsDockerDemoApplicationTests.java
@@ -1,19 +1,14 @@
 package com.aboydfd.jenkins_docker_demo;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class JenkinsDockerDemoApplicationTests {
 
     @Test
     public void contextLoads() {
-        fail();
+        assertEquals("a", "b");
     }
 
 }
