eb43bfac2b5841c272985ecc81a8c788cc951587
rlin <rlin@thoughtworks.com>
2020-02-16 09:07:09 +0800

    [@boydfd] test for docker.

--- docker/build.sh
+++ docker/build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-dockerRegistry='docker.aboydfd.com:32013'
+dockerRegistry='docker.aboydfd.com'
 imageName=jenkins_docker_spring
 cd $(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
 
 
 set -x
-docker login docker.aboydfd.com:32013 -u boydfd -p 123456
+docker login $dockerRegistry -u boydfd -p 123456
 docker build -t "$dockerRegistry/$imageName" .
 docker push "$dockerRegistry/$imageName"
 set +x
