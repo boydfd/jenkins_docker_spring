ff96c0710058db05325d87a471d6607e308f2109
rlin <rlin@thoughtworks.com>
2020-02-16 16:52:36 +0800

    [@boydfd] test for docker user/password.

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,9 @@ cd $(dirname $([ -L $0 ] && readlink -f $0 || echo $0))
 
 
 set -x
-docker login $dockerRegistry -u boydfd -p 123456
+echo $docker_user
+echo $docker_password
+docker login $dockerRegistry -u $docker_user -p $docker_password
 docker build -t "$dockerRegistry/$imageName" .
 docker push "$dockerRegistry/$imageName"
 set +x
